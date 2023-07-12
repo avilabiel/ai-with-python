e7b101a19985e9bb63f313f8a0d82728801fad39
Gabriel Ávila <avilabiel@gmail.com>
2023-07-12 02:27:02 -0300

    setup of my repo

---
+++ hello.py
@@ -0,0 +1,7 @@
+# NB: following this URL to setup Python extension
+# https://code.visualstudio.com/docs/languages/python#:~:text=To%20run%20the%20active%20Python,command%20(Shift%2BEnter).
+
+print("Hello")
+i = 0
+
+print(i)
